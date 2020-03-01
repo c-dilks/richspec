61d35cb9bd6369dd875ccb145dd4c36f4bb5266a
christopher dilks <christopher.j.dilks@gmail.com>
2020-02-29 19:44:09 -0500

    bin2hist looper works

--- convertBin.sh
+++ convertBin.sh
@@ -2,11 +2,13 @@
 if [ $# -ne 1 ]; then echo "usage: $0 [dataDir]"; exit; fi
 pushd $1
 for file in `ls *.bin`; do 
-  echo $file
-  if [ ! -f "${file}.hist.root" ]; then echo bin2hist $file; fi
+  echo "convert $file"
+  if [ ! -f "${file}.hist.root" ]; then
+    /home/drewkenjo/decode/bin2hist $file
+  fi
 done
 cd ..
 dirName=`echo $1|sed 's/^.*\///g'`
 echo $dirName
-#tar czvf ${dirName}.tar.gz ${dirName}/*.{root,log}
+tar czvf ${dirName}.tar.gz ${dirName}/*.{root,log}
 popd
